3b25bd9c0f6f0f5a975251ce052b383f01728585
tushar-sadat <tusharsadat30@gmail.com>
2024-12-19 14:24:11 +0600

    seed demo user data

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,6 +22,11 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'phone' => fake()->phoneNumber,
+            'address' => fake()->address,
+            'photo' => fake()->imageUrl('60', '60'),
+            'role' => fake()->randomElement(['admin', 'agent', 'user']),
+            'status' => fake()->randomElement(['active', 'inactive']),
             'remember_token' => Str::random(10),
         ];
     }
@@ -33,7 +38,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
