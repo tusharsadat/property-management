d07490a61b37b1319ca3a2c6326f5228921a46ac
tushar-sadat <tusharsadat30@gmail.com>
2024-12-22 12:40:27 +0600

    implement admin logout option

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,7 @@ require __DIR__ . '/auth.php';
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
 }); // End Group Admin Middleware
 
 Route::middleware(['auth', 'role:agent'])->group(function () {
