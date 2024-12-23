38603f49e2f2171fe32892f9a45de95fd305ed22
tushar-sadat <tusharsadat30@gmail.com>
2024-12-23 14:37:30 +0600

    unlink previous photo when upload new photo

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -47,6 +47,7 @@ class AdminController extends Controller
         $data->address = $request->address;
         if ($request->file('photo')) {
             $file = $request->file('photo');
+            @unlink(public_path('upload/admin_images/' . $data->photo));
             $filename = date('YmdHi') . $file->getClientOriginalName();
             $file->move(public_path('upload/admin_images'), $filename);
             $data['photo'] = $filename;
