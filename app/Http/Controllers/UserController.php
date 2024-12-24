0ce1e3ae9bae8808abccad4fe148cc68793734d9
tushar-sadat <tusharsadat30@gmail.com>
2024-12-24 13:12:02 +0600

    organize frontend template

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,6 @@ class UserController extends Controller
 {
     public function Index()
     {
-        return view('frontend.frontend_dashboard');
+        return view('frontend.index');
     } // End Method 
 }
