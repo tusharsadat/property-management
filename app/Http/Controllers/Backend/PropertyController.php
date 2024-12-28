b806c304bf47822b04c1ffc34bb0d2dcacbabab0
tushar-sadat <tusharsadat30@gmail.com>
2024-12-28 13:44:07 +0600

    working on property CRUD

--- app/Http/Controllers/Backend/PropertyController.php
+++ app/Http/Controllers/Backend/PropertyController.php
@@ -13,4 +13,9 @@ class PropertyController extends Controller
         $property = Property::latest()->get();
         return view('backend.property.all_property', compact('property'));
     } // End Method 
+
+    public function AddProperty()
+    {
+        return view('backend.property.add_property');
+    } // End Method 
 }
--- routes/web.php
+++ routes/web.php
@@ -89,5 +89,6 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
     // Property All Route 
     Route::controller(PropertyController::class)->group(function () {
         Route::get('/all/property', 'AllProperty')->name('all.property');
+        Route::get('/add/property', 'AddProperty')->name('add.property');
     });
 }); // End Group Admin Middleware
