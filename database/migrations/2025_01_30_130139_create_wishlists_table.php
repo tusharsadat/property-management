d5822fb135e8e954017df5f98a2a0efaf765d779
tushar-sadat <tusharsadat30@gmail.com>
2025-01-30 22:33:56 +0600

    create wishlist controller model and migration

---
+++ database/migrations/2025_01_30_130139_create_wishlists_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wishlists', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // User who owns the wishlist
+            $table->foreignId('property_id')->constrained()->onDelete('cascade'); // Property added to wishlist
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wishlists');
+    }
+};
