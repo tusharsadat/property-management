fb5c5580e643289951ab3194f24518b656b0b18d
tushar-sadat <tusharsadat30@gmail.com>
2025-01-31 23:50:52 +0600

    display user wishlist using ajax

--- resources/views/frontend/frontend_dashboard.blade.php
+++ resources/views/frontend/frontend_dashboard.blade.php
@@ -168,6 +168,80 @@
         }
     </script>
 
+    <!-- // start load Wishlist Data  -->
+    <script type="text/javascript">
+        function wishlist() {
+            $.ajax({
+                    type: "GET",
+                    dataType: "json",
+                    url: "/get-wishlist-property/",
+                })
+                .done(function(response) {
+                    // Update Wishlist Quantity
+                    $('#wishQty').text(response.wishQty);
+
+                    // Check if wishlist is empty
+                    if (response.wishlist.length === 0) {
+                        $('#wishlist').html(`<p class="text-center text-muted">Your wishlist is empty.</p>`);
+                        return;
+                    }
+
+                    // Build Wishlist Items
+                    let wishlistItems = response.wishlist.map(item => `
+                    <div class="deals-block-one">
+                        <div class="inner-box">
+                            <div class="image-box">
+                                <figure class="image">
+                                    <img src="/${item.property.property_thambnail}" alt="${item.property.property_name}">
+                                </figure>
+                                <div class="batch"><i class="icon-11"></i></div>
+                                <span class="category">Featured</span>
+                                <div class="buy-btn">
+                                    <a href="#">For ${item.property.property_status}</a>
+                                </div>
+                            </div>
+                            <div class="lower-content">
+                                <div class="title-text">
+                                    <h4><a href="#">${item.property.property_name}</a></h4>
+                                </div>
+                                <div class="price-box clearfix">
+                                    <div class="price-info pull-left">
+                                        <h6>Start From</h6>
+                                        <h4>$${item.property.lowest_price}</h4>
+                                    </div>
+                                </div>
+                                <ul class="more-details clearfix">
+                                    <li><i class="icon-14"></i>${item.property.bedrooms} Beds</li>
+                                    <li><i class="icon-15"></i>${item.property.bathrooms} Baths</li>
+                                    <li><i class="icon-16"></i>${item.property.property_size} Sq Ft</li>
+                                </ul>
+                                <div class="other-info-box clearfix">
+                                    <ul class="other-option pull-right clearfix">
+                                        <li><a href="property-details.html"><i class="icon-13"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                `).join('');
+
+                    // Update Wishlist UI
+                    $('#wishlist').html(wishlistItems);
+                })
+                .fail(function(xhr, status, error) {
+                    console.error("Error fetching wishlist:", error);
+                    Swal.fire({
+                        icon: "error",
+                        title: "Oops...",
+                        text: "Failed to load your wishlist. Please try again later!",
+                    });
+                });
+        }
+
+        // Load Wishlist on Page Load
+        wishlist();
+    </script>
+
 </body><!-- End of .page_wrapper -->
 
 </html>
